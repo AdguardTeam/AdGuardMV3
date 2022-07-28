76514f88cc0fed4f541b74306e23dc2682413322
Dmitriy Seregin <d.seregin@adguard.com>
2022-07-28 12:39:23 +0300

    added todo

--- src/common/translators/i18n.ts
+++ src/common/translators/i18n.ts
@@ -2,7 +2,7 @@ import { I18nInterface } from '@adguard/translate';
 
 export const i18n: I18nInterface = {
     getMessage: chrome.i18n.getMessage,
-    getUILanguage: () => 'en',
+    getUILanguage: () => 'en', // TODO: fix in https://jira.adguard.com/browse/AG-15583
     getBaseMessage: (key: string) => key,
     getBaseUILanguage: () => 'en',
 };
