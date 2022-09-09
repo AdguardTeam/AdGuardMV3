dfa55b7f39b41f7758289d6d797f0d10dd6e2ffc
Anton Kaprielov <a.kaprielov@adguard.com>
2022-09-09 10:24:20 +0300

    Pull request #156: add cross-env
    Merge in EXTENSIONS/browser-extension-v3 to master
    
    Squashed commit of the following:
    
    commit 4cc6465c5f2aaae589c8c59f87fbef2160290458
    Author: Anton <a.kaprielov@adguard.com>
    Date:   Thu Sep 8 16:01:52 2022 +0300
    
        AG-16340 add cross-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,11 @@
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 
+const postcssPresetEnv = require('postcss-preset-env');
+
 module.exports = {
     plugins: [
+        [postcssPresetEnv, { stage: 3, features: { 'nesting-rules': true } }],
         autoprefixer,
         cssnano({ preset: 'default' }),
         'postcss-import',
